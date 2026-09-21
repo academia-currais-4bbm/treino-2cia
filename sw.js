4b21648215f58b78508c01806b1e8d11adc694ac
academia-currais-4bbm <sidaomelo1992@gmail.com>
2026-09-21 06:52:44 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE='t2cia-v67.64.0';
-const RUNTIME='t2cia-runtime-v67.64.0';
+const CACHE='t2cia-v67.65.0';
+const RUNTIME='t2cia-runtime-v67.65.0';
 const SHELL=[
   './',
   'index.html',
-  'styles-v67-64-0.css',
-  'app-v67-64-0.js',
+  'styles-v67-65-0.css',
+  'app-v67-65-0.js',
   'data.js',
   'cloud-config.js',
   'manifest.json',
@@ -35,8 +35,8 @@ self.addEventListener('activate',event=>{
 function isAppShell(url){
   return url.pathname.endsWith('/') ||
     url.pathname.endsWith('/index.html') ||
-    url.pathname.endsWith('/app-v67-64-0.js') ||
-    url.pathname.endsWith('/styles-v67-64-0.css') ||
+    url.pathname.endsWith('/app-v67-65-0.js') ||
+    url.pathname.endsWith('/styles-v67-65-0.css') ||
     url.pathname.endsWith('/data.js') ||
     url.pathname.endsWith('/cloud-config.js') ||
     url.pathname.endsWith('/manifest.json');
